d1c7cd48cfc824e97be914c1d863c2f64fdfccde
3spoen <3spoen@gmail.com>
2024-03-20 00:02:20 +0100

    adding a metatags

--- app/page.js
+++ app/page.js
@@ -6,10 +6,18 @@ import ProjectsSection from "./Comp/ProjectsSection";
 import { EmailSection } from "./Comp/EmailSection";
 import { Footer } from "./Comp/Footer";
 import Achievment from "./Comp/AchievementsSection"
+import Head from "next/head";
 
 
 export default function Home() {
   return (
+    <>
+    
+    <Head>
+        <title>Omar Alali: Crafting Digital Experiences</title>
+        <meta name="description" content="Discover Omar Alali's portfolio: a blend of innovative design and tech. See how creativity and expertise shape impactful digital experiences" />
+        {/* Add other meta tags as needed */}
+      </Head>
     <main className="flex min-h-screen flex-col bg-slate-950">
     <Navbar />
     <div class="container mt-24 mx-auto px-12 py-4">
@@ -21,5 +29,7 @@ export default function Home() {
     </div>
     <Footer/>
     </main>
+    </>
+
   );
 }
