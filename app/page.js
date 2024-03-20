50575bba00b0595e82fe43024daad95d59101ac4
3spoen <3spoen@gmail.com>
2024-03-20 13:03:21 +0100

    metatags 6

--- app/page.js
+++ app/page.js
@@ -14,17 +14,22 @@ export default function Home() {
     <>
     
     <Head>
-    <title>Omar Alali&apos;s Portfolio</title>
-    <meta name="description" content="Discover Omar Alali's portfolio: a blend of innovative design and tech. See how creativity and expertise shape impactful digital experiences."/>
-    <meta property="og:type" content="website"/>
-    <meta property="og:url" content="https://www.3spoen.de/"/>
-    <meta property="og:title" content="Omar Alali's Portfolio"/>
-    <meta property="og:description" content="Discover Omar Alali's portfolio: a blend of innovative design and tech. See how creativity and expertise shape impactful digital experiences."/>
-
+    <meta charSet="utf-8"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1"/>
+        <title>Omar Alali's Portfolio</title>
+        <meta name="description" content="Discover Omar Alali's portfolio: a blend of innovative design and tech. See how creativity and expertise shape impactful digital experiences."/>
+        <link rel="icon" href="/favicon.ico" type="image/x-icon"/>
+        {/* Open Graph Meta Tags */}
+        <meta property="og:type" content="website"/>
+        <meta property="og:url" content="https://www.3spoen.de/"/>
+        <meta property="og:title" content="Omar Alali's Portfolio"/>
+        <meta property="og:description" content="Discover Omar Alali's portfolio: a blend of innovative design and tech. See how creativity and expertise shape impactful digital experiences."/>
+        {/* Add og:image tag with an absolute URL to an image */}
+        <meta property="og:image" content="https://www.3spoen.de/images/LOGO_2.png"/>
       </Head>
     <main className="flex min-h-screen flex-col bg-slate-950">
     <Navbar />
-    <div class="container mt-24 mx-auto px-12 py-4">
+    <div className="container mt-24 mx-auto px-12 py-4">
       <HeroSection/> 
       {/*<Achievment/>*/}
       <AboutSec/>
