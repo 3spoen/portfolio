99daefe8cd2323e99b1f08060c2e992686fc3500
3spoen <3spoen@gmail.com>
2024-04-16 00:18:52 +0200

    updating the cv's

--- app/Comp/HeroSection.jsx
+++ app/Comp/HeroSection.jsx
@@ -55,14 +55,14 @@ const HeroSection = () => {
                     className=" hover:bg-gradient-to-br font-bold hover:from-purple-600 hover:via-cyan-400 hover:to-blue-600 hover:text-black px-11  py-3  text-white   rounded-full inline-block w-full sm:w-fit mr-4 bg-gradient-to-br from-purple-600 via-cyan-600 to-blue-600  ">
                         {t('Hire me')}
                         </Link>
-                    <a href="https://www.3spoen.de/docs/cv_DEU.pdf" download="cv_DEU.pdf"  alt="alt text" target="_blank" rel="noopener noreferrer">
+                    <a href="https://www.3spoen.de/docs/cv_DE.pdf" download="cv_DE.pdf"  alt="alt text" target="_blank" rel="noopener noreferrer">
                         <button  className="px-1  py-1 inline-block rounded-full w-full sm:w-fit bg-gradient-to-br from-purple-600 via-cyan-600 to-blue-600 mr-4 mt-2">
                             <span className="block bg-slate-950 hover:bg-slate-800 rounded-full px-5 py-2 text-cyan-500">
                                 {t('Download CV DE')}
                             </span>
                         </button>
                     </a>
-                    <a href="https://www.3spoen.de/docs/cv_EN.pdf" download="cv_EN.pdf" alt="alt text" target="_blank" rel="noopener noreferrer" >
+                    <a href="https://www.3spoen.de/docs/cv_ENG.pdf" download="cv_ENG.pdf" alt="alt text" target="_blank" rel="noopener noreferrer" >
                         <button  className="px-1  py-1 inline-block rounded-full w-full sm:w-fit bg-gradient-to-br from-purple-600 via-cyan-600 to-blue-600  mt-2">
                             <span className="block bg-slate-950 hover:bg-slate-800 rounded-full px-5 py-2 text-cyan-500">
                                 {t('Download CV EN')}
